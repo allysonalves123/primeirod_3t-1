bff4bbfc6e68c697376da0b8b5ae2a88469a24e9
Marianna Mika <110108982+asxrangel@users.noreply.github.com>
2022-10-19 11:13:13 -0300

    Avaliação 19-10

--- estudo.js
+++ estudo.js
@@ -11,5 +11,38 @@ function total(){
     
     document.write("Resultado: " + r);
 }
-
-
+function adicao(){
+    let val1 = document.getElementById("v1").value;
+    let val2 = document.getElementById("v2").value;
+    let r = Number(val1) + Number(val2);
+    document.getElementById("resultado").innerHTML =r;
+    }
+    
+    function subtracao(){
+      let val1 = document.getElementById("v1").value;
+      let val2 = document.getElementById("v2").value;
+      let r = Number(val1) - Number(val2);
+      document.getElementById("resultado").innerHTML =r;
+      }
+    
+      function divisao(){
+        let val1 = document.getElementById("v1").value;
+        let val2 = document.getElementById("v2").value;
+        let r = Number(val1) / Number(val2);
+        document.getElementById("resultado").innerHTML =r;
+        }
+      
+        function multiplicacao(){
+          let val1 = document.getElementById("v1").value;
+          let val2 = document.getElementById("v2").value;
+          let r = Number(val1) * Number(val2);
+          document.getElementById("resultado").innerHTML =r;
+          }
+          function porcentagem(){
+            let val1 = document.getElementById("v1").value;
+            let val2 = document.getElementById("v2").value;
+            let c = 100
+            let p = Number(val2)  / Number(c)
+            let r = Number(val1) * Number(p);
+            document.getElementById("resultado").innerHTML =r;
+            }
